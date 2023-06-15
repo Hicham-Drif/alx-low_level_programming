40f82ef385fca7be3056fee5ad76000d86cfd6bd
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-15 15:03:20 -0700

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,8 @@
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 59);
+	write(1, quot, 59);
 	return (1);
 }
