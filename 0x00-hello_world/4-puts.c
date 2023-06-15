011dd783f3ae57c0307839f8bfa1adff386a440b
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-15 13:02:35 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A programe tha prints using puts
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
