11c833b72f4550da6fa90fce28d3a9ae9aae8d24
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-17 06:13:33 -0700

    new commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A programe tha prints Hexa number  in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char num= '0';
+
+	while (num <= 'f')
+	{
+	putchar(num);
+	if (num == 57)
+	{
+	num = num + 39;
+	}
+	num++;
+	}
+	putchar('\n');
+	return (0);
+}
