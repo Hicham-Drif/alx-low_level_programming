c3481191f4f2f7ad6bf5722699f79ff6cb652e87
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-17 00:59:45 -0700

    new commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Description: A c programe 
+ * Description: Print the last digit
  *
  * Return: Always 0 (Success)
 */
@@ -13,9 +13,16 @@
 int main(void)
 {
 	int n;
+	int digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+	digit = n % 10;
+	if (digit > 5)
+	printf("Last digit of %i is %i and is is greater than 5\n", n, digit);
+	if (digit == 0)
+	printf("Last digit of %i is 0 and is 0\n", n);
+	if (digit > 6 && digit != 0)
+	printf("Last digit of %i is %i and is is less than 6\n", n, digit);
 	return (0);
 }
