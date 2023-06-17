d8a0a0ebd48445fe865fe82ccdaaff333e26d710
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-17 01:21:43 -0700

    new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A programe tha prints alphabets
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+	putchar(alph);
+	alph++;
+	}
+	putchar("\n");
+	return (0);
+}
