3bfa4e92cb1d3d28d74424a43e774f38334a055c
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-17 01:53:46 -0700

    new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A programe tha prints numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+	printf("%i", num);
+	num++;
+	}
+	printf("\n");
+	return (0);
+}
