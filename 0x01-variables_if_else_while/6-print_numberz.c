04494f1471e521d03063e13aa3ff5c34cd7b1f94
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-17 02:02:38 -0700

    new commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A programe tha prints numbers using putchar
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+	putchar(num + 48);
+	num++;
+	}
+	printf("\n");
+	return (0);
+}
