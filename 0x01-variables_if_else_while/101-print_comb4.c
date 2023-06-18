a25891e09c75915deccf2a70134ae440a3beed33
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-18 09:53:27 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 
 /**
@@ -28,19 +29,18 @@ int main(void)
 				putchar(num1 + 48);
 				putchar(num2 + 48);
 				putchar(num3 + 48);
-				    if (num1 + num2 + num3 != 24)
-				    {
-				    putchar(',');
-				    putchar(' ');
-                    }
+					if (num1 + num2 + num3 != 24)
+					{
+					putchar(',');
+					putchar(' ');
+					}
 				}
-            num3++;
-            }
+			num3++;
+			}
 		num2++;
 		}
-    num1++;
+	num1++;
 	}
-	;
 	putchar('\n');
 	return (0);
 }
