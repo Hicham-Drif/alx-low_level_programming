d1c5964acf383afbb1056b7c43af81d816e65307
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-23 11:36:05 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer
+* starting from 00:00 to 23:59
+*/
+
+void jack_bauer(void)
+{
+	int hrs,min;
+
+	for (hrs = 0; hrs < 24 ; hrs++)
+        {
+		for (min = 0; min < 60; min++)
+		{
+                	_putchar((hrs / 10) + 48);
+                	_putchar((hrs % 10) + 48);
+                	_putchar(':');
+                	_putchar((min / 10) + 48);
+                	_putchar((min % 10) + 48);
+			_putchar('\n'); 			
+		}
+	}
+}
