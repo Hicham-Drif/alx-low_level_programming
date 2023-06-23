7f27fe838b20721c764e2f9ec9761cf30157f704
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-23 00:15:05 -0700

    new commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Description: A programe tha prints alphabets
+ * Description: A programe tha prints _putchar using predifined function _putchar
  *
  * Return: Always 0 (Success)
 */
