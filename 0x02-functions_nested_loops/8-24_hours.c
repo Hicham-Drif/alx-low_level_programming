2fb351d61074ea0f8c4a884f84c4c63a83a0fa17
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-23 11:40:45 -0700

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,18 @@
 
 void jack_bauer(void)
 {
-	int hrs,min;
+	int hrs, min;
 
-	for (hrs = 0; hrs < 24 ; hrs++)
-        {
+	for (hrs = 0; hrs < 24; hrs++)
+	{
 		for (min = 0; min < 60; min++)
 		{
-                	_putchar((hrs / 10) + 48);
-                	_putchar((hrs % 10) + 48);
-                	_putchar(':');
-                	_putchar((min / 10) + 48);
-                	_putchar((min % 10) + 48);
-			_putchar('\n'); 			
+			_putchar((hrs / 10) + 48);
+			_putchar((hrs % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
 		}
 	}
 }
