05ba990bcda9c623c8a9c1ae8e8c76cd286e4dc8
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-23 10:56:48 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,14 @@
  */
 int print_last_digit(int n)
 {
-        int lastdigit;
+	int lastdigit;
 
-        if (n < 0)
-        {
-        lastdigit = -1 * (n % 10);
-        }
-        else
-        lastdigit = n % 10;
-        _putchar(lastdigit + '0');
-        return(lastdigit);
+	if (n < 0)
+	{
+	lastdigit = -1 * (n % 10);
+	}
+	else
+	lastdigit = n % 10;
+	_putchar(lastdigit + '0');
+	return(lastdigit);
 }
