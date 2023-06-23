a186082432d5cad3f23a620e448af630aeb2fa2d
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-23 00:57:54 -0700

    new commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* * print_alphabet_x10 - print all alphabet in lowercase
+*using _putchar function
+*
+* Return: (void)
+*/
+void print_alphabet_x10(void)
+{
+	int i = 'a';
+	int count = 0;
+
+	while (count < 10)
+	{
+	i = 'a';
+	while (i <= 'z')
+	{
+	_putchar(i);
+	i++;
+	}
+	count++;
+	_putchar('\n');
+	}
+}
