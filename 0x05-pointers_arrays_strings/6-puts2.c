fcdd26548347e0b715b6220036555cfe88d1d59f
Hicham-Drif <hicham.drif@hotmail.fr>
2023-06-28 07:40:26 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if ((str[i] % 2 == 0 ))
+	{
+		_putchar(str[i]);
+	}
+}
+_putchar('\n');
+}
