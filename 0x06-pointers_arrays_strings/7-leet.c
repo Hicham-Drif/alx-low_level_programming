02276fa1083d84c128394ee6f4ca606c93ea2056
Hicham-Drif <hicham.drif@hotmail.fr>
2023-07-03 23:25:28 -0700

    a

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,6 @@ char *leet(char *n)
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
-
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -17,7 +16,7 @@ char *leet(char *n)
 			if (n[i] == s1[j])
 			{
 				n[i] = s2[j];
-			}
+            }
 		}
 	}
 	return (n);
