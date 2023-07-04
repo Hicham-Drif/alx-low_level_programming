6a4fa505053b704af0db8430cecdb67c9d801267
Hicham-Drif <hicham.drif@hotmail.fr>
2023-07-04 04:25:25 -0700

    new commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,10 @@
 */
 
 char *_memset(char *s, char b, unsigned int n)
-
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    for (;i < n; i++)
-        s[i] = b;
-    
-    return (s);
+	for (;i < n; i++)
+		s[i] = b;  
+	return (s);
 }
