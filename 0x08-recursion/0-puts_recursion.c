aa026f0a1891765fd5e90afc41c195344024b82d
Hicham-Drif <hicham.drif@hotmail.fr>
2023-07-08 09:06:24 -0700

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+_puts_recursion(char *s)
+{
+	if (*s == '/0')
+	return (0);
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+	else
+	_putchar('\n');
+}
