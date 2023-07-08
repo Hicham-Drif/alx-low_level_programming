e4753926d9d5ab4bfdfac959a28ba15a7078ab11
Hicham-Drif <hicham.drif@hotmail.fr>
2023-07-08 09:20:21 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
-#include <unistd.h>
-/*
-* _print_rev_recursion - Prints a string in reverse.
-* @s: The string to be printed.
-*/
-
+#include <unistd>
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
