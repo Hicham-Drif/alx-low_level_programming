5705a043f3ffa92af6f5db54bd733c084d93897d
Hicham-Drif <hicham.drif@hotmail.fr>
2023-07-09 02:48:51 -0700

    a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isupper - check if char is in uppercase
+ * @c: is the char to be checked
+ * Return: 1 if char is in uppercase, otherwise 0.
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	return (1);
+	else
+	return (0);
+	_putchar('\n');
+}
