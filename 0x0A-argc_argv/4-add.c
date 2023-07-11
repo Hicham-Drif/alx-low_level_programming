93e350916c4846177e9ac1acd43b1373fea3a9a5
Hicham-Drif <hicham.drif@hotmail.fr>
2023-07-10 22:51:50 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,14 +16,15 @@ int isnumber (char *c);
 int main(int argc, char *argv[])
 {
 	int i, strtonum,  sum = 0;
-	for (i = 1; i < argc; i++ )
+
+	for (i = 1; i < argc; i++)
 	{	if (isnumber(argv[i] == 0)
 		{
 			strtonum = atoi(argv[i]);
 			sum += strtonum;
 		}
 	}
-	
+
 	printf("%d\n", sum); /*print sum*/
 
 	return (0);
@@ -37,14 +38,15 @@ int main(int argc, char *argv[])
 */
 int isnumber (char *c)
 {
-	int len = 0, i  = 0;
+	int len = 0, i = 0;
+	
 	while (c[len] != '\0')
 	{
 		len++;
 	}
-	for (;i < len; i++)
+	for (; i < len; i++)
 	{
-		if (isdigit(c[i]) == 0)	
+		if (isdigit(c[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);
@@ -53,6 +55,5 @@ int isnumber (char *c)
 		{
 			return (0);
 		}
+	}
 }
-
-
