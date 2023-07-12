88ac5d156e963bdaca7af3ae494cd1ee64bb1ae3
Hicham-Drif <hicham.drif@hotmail.fr>
2023-07-12 14:07:29 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,52 +2,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-int isnumber(char *c);
+#include <string.h>
 
 /**
-* main - add two numbers
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: 0 (Success), 1 (Error)
-*/
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+
+int check_num(char *str)
+{
+
+        /*Declaring variables*/
+
+        unsigned int count;
+
+
+        count = 0;
+
+        while (count < strlen(str)) /*count string*/
+
+
+        {
+
+                if (!isdigit(str[count])) /*check if str there are digit*/
+
+                {
+
+                        return (0);
+
+                }
+
+
+                count++;
+
+        }
+
+        return (1);
+
+}
+
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 {
-	int i, strtonum,  sum = 0;
 
-	for (i = 1; i < argc; i++)
-	{
-		if (isnumber(argv[i]) == 0)
-		{
-			strtonum = atoi(argv[i]);
-			sum += strtonum;
-		}
-	}
 
-	printf("%d\n", sum); /*print sum*/
+        /*Declaring variables*/
 
-	return (0);
-}
+        int count;
 
-/**
-* isnumber - verify if it's a number
-* @c: character to verify
-* Return: 0 (Success), 1 (Error)
-*/
+        int str_to_int;
 
-int isnumber(char *c)
-{
-	int i = 0;
+        int sum = 0;
 
-	while (c[i] != '\0')
-	{
-		if (!isdigit(c[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-	i++;
-	}
-	return (0);
+
+        count = 1;
+
+        while (count < argc) /*Goes through the whole array*/
+
+        {
+
+                if (check_num(argv[count]))
+
+
+                {
+
+                        str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+
+                        sum += str_to_int;
+
+                }
+
+
+                /*Condition if one of the number contains symbols that are not digits*/
+
+                else
+
+                {
+
+                        printf("Error\n");
+
+                        return (1);
+
+                }
+
+
+                count++;
+
+        }
+
+
+        printf("%d\n", sum); /*print sum*/
+
+
+        return (0);
+
 }
