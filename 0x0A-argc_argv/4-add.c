c83ea02d12d7148c23e7f4d11e412d44140420cb
Hicham-Drif <hicham.drif@hotmail.fr>
2023-07-12 21:35:25 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,61 +1,33 @@
-#include "main.h"
-#include <stdio.h>
+#include2 <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
- * check_num - check - string there are digit
- * @str: array str
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vecctor arrray
  *
- * Return: Always 0 (Success)
- */
-
-int check_num(char *str)
-{
-	unsigned int count;
-
-	count = 0;
-
-	while (count < strlen(str))
-        {
-		if (!isdigit(str[count]))
-		{
-			return (0);
-		}
-	count++;
-	}
-	return (1);
-}
-
-/**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * Return: Always 0 (Success)
+ * Return: 0;
  */
 
 int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
-	int sum = 0;
+	int i, sum = 0;
 
-	count = 1;
-	while (count < argc)
+	if (argc == 1)
 	{
-		if (check_num(argv[count]))
-		{
-			str_to_int = atoi(argv[count]);
-			sum += str_to_int;
-		else
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
-			count++;
-		}
-		printf("%d\n", sum); /*print sum*/
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n",  sum);
 	return (0);
 }
