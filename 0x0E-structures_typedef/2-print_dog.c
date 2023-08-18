5e641cd26f67df055811b6daf61e5e4e9f11d9cc
hicham drif <hicham.drif@hotmail.fr>
2023-08-18 16:07:45 -0700

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,11 +7,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	if ((*d).name == NULL)
-		(*d).name = "(nil)";
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
