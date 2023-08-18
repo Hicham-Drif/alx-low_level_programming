c761dde70bd12b3396de4c96b17d41b84d886961
hicham drif <hicham.drif@hotmail.fr>
2023-08-18 05:52:48 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,15 @@
 #define DOG_H
 
 
-
 /**
-* struct dog - Short description
-* @name: First member char*
-* @age: Second member float
-* @owner: Third member char*
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
 *
 * Description: Longer description
 */
+
 struct dog
 {
 	char * name;
