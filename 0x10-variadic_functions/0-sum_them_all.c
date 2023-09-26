3eabdb9ecf9bada50c503bbeda063b143405b19f
Hicham <hicham.drif@hotmail.fr>
2023-09-26 08:00:04 -0700

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	if( n==0)
 		return(0);
-	int i, sum = 0;
+	unsigned int i, sum = 0;
 	va_list args;
 	
 	va_start(args, n);
-	for (i=0; i<n; i++)
+	for (i=0; i < n; i++)
 		sum += va_args(args,int);
+	
+	va_end(args);
 	return(sum);
 }
