c0cf06eb3664fa3fc754d0ab27f003ee3b1472c2
Hicham-Drif <hicham.drif@hotmail.fr>
2023-11-01 18:26:54 -0700

    my commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,8 +8,8 @@
  * @height: height dimension of grid
  * Description: frees memory of grid
  * Return: nothing
- */		
-void free_grid(int **grid, int height)		
+ */
+void free_grid(int **grid, int height)
 {
 	int i;
 
