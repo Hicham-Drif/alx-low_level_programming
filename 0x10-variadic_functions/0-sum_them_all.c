c340042f585044fc6eef4041e5477dab51cadad7
Hicham-Drif <hicham.drif@hotmail.fr>
2023-11-09 07:35:14 -0800

    variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,22 @@
-#include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
-
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * sum_them_all - adds all args
+ * @n: num of args
+ * Return: result
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sum = 0;
-	va_list args;
+	va_list valist;
+	unsigned int sum;
+	unsigned int i;
 
+	sum = 0;
+	va_start(valist, n);
 	if (n == 0)
 		return (0);
-
-	va_start(args, n);
-
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
-
-	va_end(args);
+		sum += va_arg(valist, unsigned int);
+	va_end(valist);
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,21 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
+
 /**
- * print_numbers - Prints numbers, followed by a new line.
- * @separator: The string to be printed between numbers.
- * @n: The number of integers passed to the function.
- * @...: A variable number of numbers to be printed.
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: string to be printed between numbers
+ * @n: num of int
+ * Return: ntg
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list valist;
 	unsigned int i;
-	va_list nums;
-	
-	va_start(nums, n);
 
-	for (i=0; i < n; i++)
-		printf("%d",va_arg(nums, int))
-		if (separator != NULL && i!= n-1) 
-			printf("%s ",separator);
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1) && separator != NULL)
+			printf("%d%s", va_arg(valist, unsigned int), separator);
+		else
+			printf("%d", va_arg(valist, unsigned int));
+	}
 	printf("\n");
-	va_end(nums);
+	va_end(valist);
 }
