9cc4987bfd17073a2e49b3346af8644ac1560626
Hicham-Drif <Hicham.drif@hotmail.fr>
2023-11-17 07:12:46 -0800

    update betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,11 @@ void free_list(list_t *head)
 {
  list_t *temp;
 
- while (head)
- {
- temp = head->next;
- free(head->str);
- free(head);
- head = temp;
- }
+ 	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
-
